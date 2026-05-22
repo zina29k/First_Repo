9a8707f5c7629848713f9f18caf763802a317d04
zina29k <zina29k@gmail.com>
2026-05-22 13:29:59 +0200

    render fix

--- render.R
+++ render.R
@@ -7,7 +7,7 @@ ins.mat <- installed.packages()
 missing.pkgs <- setdiff(pkgs, rownames(ins.mat))
 
 if (length(missing.pkgs) > 0) {
-  install.packages(missing.pkgs, repos = "https://cloud.r-project.org")
+  install.packages(missing.pkgs)
 }
 
 quarto::quarto_render()
